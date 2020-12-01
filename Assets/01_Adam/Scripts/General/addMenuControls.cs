6c9db5823622db5b91c3479888fc4cb9c7e3ea52
Adam-Sinclair <AdamWil@ColoState.edu>
2020-12-01 09:22:59 -0700

    1

---
+++ Assets/01_Adam/Scripts/General/addMenuControls.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using UnityEngine.UI;
+
+
+public class addMenuControls : MonoBehaviour
+{
+    #region CUSTOM
+
+#if DESKTOP
+
+#elif HL2
+
+
+
+#elif VIVE
+
+    //[HideInInspector]
+    //public static SteamVR_Behaviour_Pose NonDominantDeviceBehaviourPose;
+
+    //private void Start()
+    //{
+    //    NonDominantDeviceBehaviourPose = FindObjectsOfType<SteamVR_Behaviour_Pose>().First(x => x.inputSource == SteamVR_Input_Sources.LeftHand);
+
+    //    this.gameObject.AddComponent<SteamVR_Behaviour_Pose>();
+
+    //}
+
+    [HideInInspector]
+    public static SteamVR_Behaviour_Pose PrimaryDeviceBehaviourPose;
+
+    [SerializeField]
+    public GameObject contollerPrefab;
+
+    private GameObject controller;
+
+    /// <summary>Provide the SteamVR_Input_Source related to the non-dominant controller.</summary>
+
+    /// <summary>Assign the SteamVR Behaviour Poses for each hand.</summary>
+    /// <remarks>
+    /// By default, the right hand is set as the primary device and the left hand is the non-dominant
+    /// device to avoid null reference exceptions.
+    /// </remarks>
+    private void Start()
+    {
+        controller = Instantiate(contollerPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        SteamVR_Actions.logitechToolkit_PrimaryClick.AddOnStateDownListener(TriggerPressed, SteamVR_Input_Sources.LeftHand);
+        
+    }
+
+    private void TriggerPressed(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        //GameObject[] buttonTagedObjects = GameObject.FindGameObjectsWithTag("Button");
+        //Debug.Log(buttonTagedObjects.Length);
+        //foreach (GameObject obj in buttonTagedObjects)
+        //{
+        //    Button btn = obj.GetComponent<Button>();
+        //    if (btn != null)
+        //    {
+        //        btn.Select();
+        //        btn.onClick.Invoke();
+        //        Debug.Log(btn.name);
+        //    }
+        //}
+        //Debug.Log(fromSource);
+
+
+    }
+
+#else
+
+
+#endif
+    #endregion
+}
---
+++ Assets/01_Adam/Scripts/General/tmp/MP_InputSetup.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+namespace Photon_IATK
+{
+
+    public class MP_InputSetup : MonoBehaviour
+    {
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log(GlobalVariables.green + "Setting up room inputs" + GlobalVariables.endColor + " : " + this.GetType().Name);
+            setup();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+#if DESKTOP
+
+#elif HL2
+
+#elif VIVE
+
+        private void setup()
+        {
+            SteamVR_Behaviour_Pose SteamVR_Behaviour_Pose;
+            SteamVR_Behaviour_Pose = this.gameObject.AddComponent<SteamVR_Behaviour_Pose>();
+
+            SteamVR_Behaviour_Single SteamVR_Behaviour_Single;
+            SteamVR_Behaviour_Single = this.gameObject.AddComponent<SteamVR_Behaviour_Single>();
+
+            SteamVR_Behaviour_Vector2 SteamVR_Behaviour_Vector2;
+            SteamVR_Behaviour_Vector2 = this.gameObject.AddComponent<SteamVR_Behaviour_Vector2>();
+
+            SteamVR_Behaviour_Vector3 SteamVR_Behaviour_Vector3;
+            SteamVR_Behaviour_Vector3 = this.gameObject.AddComponent<SteamVR_Behaviour_Vector3>();
+
+            SteamVR_Behaviour_Skeleton SteamVR_Behaviour_Skeleton;
+            SteamVR_Behaviour_Skeleton = this.gameObject.AddComponent<SteamVR_Behaviour_Skeleton>();
+
+        }
+
+#else
+
+#endif
+    }
+}
+
